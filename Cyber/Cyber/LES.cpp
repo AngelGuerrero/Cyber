4274eaf3035a4c12da72161b2a9363af836705f4
Ángel Guerrero <_angelguerrero_@outlook.com>
2016-03-22 01:31:18 -0600

    Agregando las funciones base de una lista enlazada simple
    He agregado las funciones:
    -AgregarElemento()
    -getElementos()
    -esta_Vacio()
    -getTotalElementos()

---
+++ Cyber/Cyber/LES.cpp
@@ -0,0 +1,80 @@
+#include "LES.h"
+
+
+#include <cstdlib>	// delete
+#include <iostream>
+
+
+using namespace std;
+
+
+LES::LES()
+{
+	ptr_PrimerElemento = ptr_UltimoElemento = ptr_ElementoAnterior = nullptr;
+}
+
+LES::~LES()
+{
+}
+
+
+// Funciones de modificación ---
+void LES::AgregarElemento(computador objeto)
+{
+	Nodo *NuevoElemento = new (nothrow) Nodo(objeto);
+	if (esta_vacio())
+	{
+		ptr_PrimerElemento = ptr_UltimoElemento = NuevoElemento;
+	}
+	else
+	{
+		ptr_UltimoElemento->NodoSiguiente = NuevoElemento;
+		ptr_UltimoElemento = NuevoElemento;
+	}
+}
+
+
+// Funciones de acceso ---
+
+// Retorna los elementos de la lista en un arreglo
+void LES::getElementos()
+{
+	computador arreglo[20];
+	if (esta_vacio())
+	{
+		cout << "La lista está vacía" << endl;
+	}
+	else
+	{
+		Nodo *Iterador = ptr_PrimerElemento;
+		for (int i = 1; Iterador != nullptr; i++)
+		{
+			cout << "Identificador: " << Iterador->Computador.getId() << endl;
+			Iterador = Iterador->NodoSiguiente;
+		}
+		delete(Iterador);
+	}
+}
+
+
+//Verifica que la lista esté vacía o no
+bool LES::esta_vacio()
+{
+	return (ptr_PrimerElemento == nullptr);
+}
+
+
+// Cuenta el total de elementos en la lista
+int  LES::getTotalElementos()
+{
+	Nodo *Iterador = ptr_PrimerElemento;
+	int total;										// Suma el total de elementos
+
+	for (total = 0; Iterador->NodoSiguiente != nullptr; total++)
+	{
+		Iterador = Iterador->NodoSiguiente;
+	}
+	delete(Iterador);
+
+	return total;
+}
---
+++ Cyber/Cyber/LES.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Nodo.h"
+
+// Lista Enlazada simple
+class LES
+{
+public:
+	Nodo *ptr_PrimerElemento;
+	Nodo *ptr_UltimoElemento;
+	Nodo *ptr_ElementoAnterior;
+
+	LES();
+	~LES();
+
+// Modificadores
+	void AgregarElemento(computador objeto);			// Recibe el objeto para agregar a la lista, si la función retorna false, el elemento no se agregó a la lista.
+
+// Funciones de acceso
+	// Muestra todos los elementos de la lista
+	void getElementos();								// Muestra todos los elementos listados
+	bool esta_vacio();
+	int getTotalElementos();
+
+};
+
